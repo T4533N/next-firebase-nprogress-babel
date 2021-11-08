efbefb06aed0b146d82c4db2c92a7308c6936cab
Taseen Tanvir <taseentanvir@gmail.com>
2021-11-08 07:11:00 +0600

    Revert "ib ui"
    This reverts commit 8de2bae80da7583778ac948ddb3b34aba9a219fe.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    backgroundColor: (theme) => ({
-      ...theme('colors'),
-      'primary-black': '#141A16',
-      'primary-white': '#F6F4F2',
-    }),
-
-    extend: {
-      backgroundImage: {
-        'wojack-think': "url('/images/wojack-think.png')",
-        'flower-frame': "url('/images/frame.png')",
-      },
-
-      height: {
-        '30-rem': '30rem',
-        '32-rem': '32rem',
-      },
-
-      width: {
-        '30-rem': '30rem',
-        '39-rem': '39rem',
-      },
-
-      colors: {
-        'primary-black': '#141A16',
-        'primary-white': '#F6F4F2',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    // require('daisyui')
-  ],
+  plugins: [],
 }
